490c28fac9d58183882569efec9e591bf8a1b302
Steve <Stevegreen22@me.com>
2016-01-30 23:08:58 +0000

    merging

--- src/test/scala/com/sessioncam/AppTest.scala
+++ src/test/scala/com/sessioncam/AppTest.scala
@@ -1,17 +1,35 @@
-package com.sessioncam
+import com.typesafe.scalalogging.{LazyLogging, Logger}
+import org.mockito.Mockito
+import org.mockito.Mockito._
+import org.scalatest.Matchers
+import org.slf4j.{Logger => Underlying}
 
-import org.junit._
-import Assert._
-
-@Test
-class AppTest {
-
-    @Test
-    def testOK() = assertTrue(true)
-
-//    @Test
-//    def testKO() = assertTrue(false)
-
+class Testable extends LazyLogging {
+    def foo() = {
+        logger.info("Foo has been called")
+    }
 }
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
+
+@RunWith(classOf[JUnitRunner])
+class  LoggerTest
+  extends FunSuite with Matchers with BeforeAndAfterEach {
+
+
+    def initTestable(mocked: Underlying): Testable = {
+        new Testable() {
+            override lazy val logger = Logger(mocked)
+        }
+    }
+
+    test("the mockito stuff") {
+        val mocked = Mockito.mock(classOf[Underlying])
+        when(mocked.isInfoEnabled()).thenReturn(true)
+        initTestable(mocked).foo()
+        verify(mocked).info("Foo has been called")
+    }
+}
